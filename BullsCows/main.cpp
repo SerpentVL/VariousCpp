340111560e706a73114f8faeaf6aa14d8f60e60b
WLaptew <wlaptew@yandex.ru>
2024-12-03 15:53:25 +0400

    main

--- BullsCows/main.cpp
+++ BullsCows/main.cpp
@@ -12,10 +12,13 @@
 using namespace std;
 
 using M = map<int, int>;
-int main()
-{
+int main(){
     cout << "Bulls and Cows\n";
-
+    const string colors ("RGB");
+    M cm, gm;
+    int cok, pok = 0;
+    string comb(4,'.'),
+           guess;
 
 
     return 0;

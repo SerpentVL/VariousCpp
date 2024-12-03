3385b481ba92cc01741be5497e71ff858a013a89
WLaptew <wlaptew@yandex.ru>
2024-12-03 15:45:56 +0400

    First commit main

--- BullsCows/main.cpp
+++ BullsCows/main.cpp
@@ -1,12 +1,21 @@
 /// Быки и коровы - консоль
-#include <iostream>
+/// Герб Саттер. Решение сложных задач на С++. Задча 1.18.
 
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+// -----
+#include <ctime>
+#include <cstdlib>
+// -----
 using namespace std;
 
+using M = map<int, int>;
 int main()
 {
-    cout << "Bulls and Cows" << endl;
-    cout << "Bulls and Cows" << endl;
+    cout << "Bulls and Cows\n";
+
 
 
     return 0;

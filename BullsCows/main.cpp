03325d46e7e8d6dd2e8972d486d51443b2c4ce8d
WLaptew <wlaptew@yandex.ru>
2024-12-04 21:45:05 +0400

    update main

--- BullsCows/main.cpp
+++ BullsCows/main.cpp
@@ -12,6 +12,16 @@
 using namespace std;
 
 using M = map<int, int>;
+
+class ChoosePeg{
+};
+
+class CountPlace{
+};
+
+class CountColor{
+};
+
 int main(){
     cout << "Bulls and Cows\n";
     const string colors ("RGB");
@@ -19,7 +29,20 @@ int main(){
     int cok, pok = 0;
     string comb(4,'.'),
            guess;
+    /// инициализация гсч
+    generate(comb.begin(), comb.end(), ChoosePeg(colors));
 
-
+    while(pok < comb.size())
+        cout << "\n\nguess-->",
+        cin >> guess,
+        guess.resize(comb.size()),      /// это поменять
+        cm = gm = M(),
+        transform(comb.begin(), comb.end(),
+                  guess.begin(), guess.end(),
+                  CountPlace(cm,gm,pok)),
+        for_each(colors.begin(), colors.end(),
+                 CountColor(cm,gm,cok)),
+        cout << cok << ' ' << pok;
+    cout << " - solved!\n";
     return 0;
 }
